52db956ccbb644b66792ebbf983f160ac17948ef
Dkresge3 <Daniellerkresge@gmail.com>
2023-05-06 16:29:54 -0400

    created db

--- app/app.py
+++ app/app.py
@@ -2,11 +2,15 @@ from flask import Flask
 from database import db
 from routes import bp
 
+
+
 app = Flask(__name__)
 app.config.from_object('config.Config')
 db.init_app(app)
 app.register_blueprint(bp)
-
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
+
     app.run(debug=True)
