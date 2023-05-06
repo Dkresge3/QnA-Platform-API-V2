18a6d1b1b0d5d22e226645f740271832aaec047d
Dkresge3 <Daniellerkresge@gmail.com>
2023-05-06 14:36:14 -0400

    stub updates

--- app/app.py
+++ app/app.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify, request
-from app import db
-from app.models import Question, Quiz, QuizQuestion
+from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
+# from model import Question, Quiz, QuizQuestion
+from database import db
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
+CORS(app)
 db.init_app(app)
 
-# Define your API routes here.
+# ... Define your API routes here ...
